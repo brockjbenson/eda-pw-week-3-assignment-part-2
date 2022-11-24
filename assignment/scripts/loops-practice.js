f81f38461cd8920a7bf4e5f1b97df119e0d52e3b
Brock Benson <brockjbenson@outlook.com>
2022-11-24 11:09:38 -0600

    up to 1c

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,10 +13,20 @@ for (let i = 0; i < 4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+//   - We would need to change the i to be less than 6 
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+  
+}
 
-// 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
+//.6TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+// start i at 3, while i is < 6, and then code in the {} then add 1 to i (i++)
+for (let i = 3; i < 6; i++) {
+  console.log(i);
+  
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
